68c6abd702e3bd38cadeed6c87c174b61b03a2f8
Jung Ho Lee <alexxleee@gmail.com>
2020-12-31 23:18:34 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   # Add your solution here
   maxnum = 0;
-  for i in 0..array.length 
+  for i in 0...array.length 
     if maxnum < array[i]
       maxnum = array[i]
     end
